91ffe7e8d7aa0dae1f8997aadae1fe117e2fe724
torokrolf <torokrolf@gmail.com>
2026-01-08 19:18:24 +0100

    Update ddns-force-update.sh

--- 11-Scripts/pfsense/ddns-force-update.sh
+++ 11-Scripts/pfsense/ddns-force-update.sh
@@ -1,1 +1,14 @@
+#!/bin/sh
 
+# Get current public IP address
+NEW_IP=$(curl -s https://checkip.amazonaws.com/)
+[ -z "$NEW_IP" ] && exit 1
+
+# Read last known IP (if exists)
+OLD_IP=$(cat /var/db/current_ip.txt 2>/dev/null)
+
+# Trigger DDNS update only if IP has changed
+if [ "$NEW_IP" != "$OLD_IP" ]; then
+  echo "$NEW_IP" > /var/db/current_ip.txt
+  /etc/rc.dyndns.update
+fi
